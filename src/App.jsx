039defb4c91a419b22b6cd5371b0f16a9409bf25
Harry Watson <harrywatson270@gmail.com>
2025-11-13 19:33:07 +0000

    created login and register page, updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Home.jsx'
 import ChatPage from './ChatPage.jsx'
 import History from './History.jsx'
+import Login from './Login.jsx'
+import Register from './Register.jsx'
+
 
 export default function App() {
   return (
@@ -9,6 +12,8 @@ export default function App() {
       <Route index element={<Home />} />
       <Route path="/chat" element={<ChatPage />} />
       <Route path="/history" element={<History />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+
+
+// harry watson will be working on this file next
+export default function Login(){
+    return(
+        <div>Login</div>
+    )
+}
