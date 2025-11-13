7b0b0c23c09015376b57d479f6005e38240446dd
oli4406 <oliver.wall1@btinternet.com>
2025-11-13 14:57:21 +0000

    Added chat history, log in/out, settings buttons to navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,10 +1,18 @@
 import {Link} from 'react-router';
 
+const loggedIn = true; // Placeholder for actual authentication logic
+
 export default function NavBar() {
     return(
         <nav>
-            <Link to="/">Home</Link><br />
-            <Link to="/chat">Chat</Link>
+            <Link style={{display:'block',margin: '10px auto'}} to="/">Home</Link>
+            <Link style={{display:'block',margin: '10px auto'}} to="/chat">Chat</Link>
+            {loggedIn && <Link style={{display:'block',margin: '10px auto'}} to="/history">History</Link>}
+
+            <div className='navButtons'>
+                <button>{ loggedIn ? 'Log out' : 'Login/Signup'}</button>
+                <button>Settings</button>
+            </div>
         </nav>
     );
 }
